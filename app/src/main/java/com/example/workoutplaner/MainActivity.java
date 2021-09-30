88a0e4515c3a5b191083e3a6d2250af97fcdd56e
Lukas Velička <lukasv093@gmail.com>
2021-09-30 13:59:24 +0300

    Testing differences

--- app/src/main/java/com/example/workoutplaner/MainActivity.java
+++ app/src/main/java/com/example/workoutplaner/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.workoutplaner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Button open_nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // paprastas button, kuris yra main activity
+        /*open_nav = (Button) findViewById(R.id.button);    *mygtukas, kol kas istrintas, tai neveiks be button id
+        open_nav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNavigation();       // atidaro navigation
+
+            }});*/
     }
+
+    // navigation atidarymo metodas
+    /*public void openNavigation() {
+        Intent intent = new Intent(this, Navigation.class);
+        startActivity(intent);
+    }*/
+
 }
--- app/src/main/java/com/example/workoutplaner/Navigation.java
+++ app/src/main/java/com/example/workoutplaner/Navigation.java
@@ -9,6 +9,6 @@ public class Navigation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation2);
+        setContentView(R.layout.activity_navigation);
     }
 }
