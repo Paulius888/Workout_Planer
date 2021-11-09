e2644b236bc3d32e5ce313b10a994a7fb45a1387
Dalius Cvilikas <daliusc@wix.com>
2021-11-09 10:24:36 +0200

    Exercise

--- app/src/main/java/com/example/workoutplaner/Exercises/RegularExerciseFragment.java
+++ app/src/main/java/com/example/workoutplaner/Exercises/RegularExerciseFragment.java
@@ -1,5 +1,8 @@
 package com.example.workoutplaner.Exercises;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,18 +10,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.workoutplaner.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
 
 public class RegularExerciseFragment extends Fragment {
 
     private Exercise exercise;
     private ArrayList<ExerciseInput> exerciseInputs;
+    AnimationDrawable avd;
 
     SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView recyclerView;
@@ -39,6 +47,15 @@ public class RegularExerciseFragment extends Fragment {
         recyclerView.setAdapter(regAdapter);
         exercise = (Exercise) getArguments().getSerializable("exercise");
         exerciseInputs = (ArrayList<ExerciseInput>)getArguments().getSerializable("regularExercisesList");
+        ImageView done = rootView.findViewById(R.id.imageView3);
+        try {
+            avd = (AnimationDrawable) done.getDrawable();
+            avd.start();
+            Log.e("OKAY", "OKAY!!");
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage());
+        }
 
         for (int i = 0; i < exercise.getSets(); i++){
             exerciseInputs.add(null);
